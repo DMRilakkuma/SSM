450c51ddf5e725e1528af46958719b643fd3b523
MC <1182002995@qq.com>
2022-08-05 22:25:57 +0800

    Changes

---
+++ spring_mvc_helloworld/src/main/java/com/atguigu/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.atguigu.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author: MC
+ * @program: SSM
+ * @create: 2022-08-05 20:36
+ * @Description:
+ */
+
+@Controller
+public class HelloController {
+
+}
